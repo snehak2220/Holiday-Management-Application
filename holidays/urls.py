259af044b014b70fe3ad174127a69e7f39b81943
SNEHA K <74949282+snehak2220@users.noreply.github.com>
2025-02-13 15:33:45 +0530

    added

---
+++ holidays/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+
+from .views import HolidayListView
+
+urlpatterns = [
+
+    path('', views.index, name="index"),
+    path('api/holidays/', HolidayListView.as_view(), name='holiday-list'),
+]`git
